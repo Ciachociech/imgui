ac9794f703e0e463d1f08e5a810cb923d88ca8ec
Mateusz Chojnacki <matchojnacki@protonmail.com>
2024-06-26 17:53:43 +0200

    Customize CMakeFile (#1)
    Customize cmake

---
+++ gcc.sh
@@ -0,0 +1,5 @@
+rm -rf build
+mkdir build
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../utils/cmake_toolchain_gcc.cmake
+cmake --build .
---
+++ utils/cmake_toolchain_clang.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_C_COMPILER clang-18)
+set(CMAKE_CXX_COMPILER clang++-18)
