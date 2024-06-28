e1477f7f526e3831d5d71a33a1e59fbdc0301986
Mateusz Chojnacki <matchojnacki@protonmail.com>
2024-06-28 17:06:38 +0200

    Separate include files (#2)
    * Separate include files

--- examples/example_glfw_opengl3/main.cpp
+++ examples/example_glfw_opengl3/main.cpp
@@ -7,7 +7,7 @@
 // - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
 // - Introduction, links and more at the top of imgui.cpp
 
-#include "imgui.h"
+#include "include/imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
